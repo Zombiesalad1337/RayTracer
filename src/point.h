860919cb3a9b582da7519b281d87e9b4a9a9da96
Zombiesalad1337 <rohit01031999@gmail.com>
2024-10-22 16:53:13 +0530

    updated point comments

--- src/point.h
+++ src/point.h
@@ -6,10 +6,10 @@ namespace rt{
 
 class Point : public Tuple{
 public:
-    //TODO: Do I need a const ref tuple constructor here?
     explicit Point(float x, float y, float z);
     Point(const Tuple& tup);
     Point(Tuple&& tup);
+    //no semantic sense in scaling a point
     Point operator*(float) = delete;
     Point operator/(float) = delete;
 };
