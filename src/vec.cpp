9e47ec0dc3ffc25877d155bcf0c7295821e58a38
Zombiesalad1337 <rohit01031999@gmail.com>
2024-11-20 13:16:58 +0530

    explained move semantics in vec

--- src/vec.cpp
+++ src/vec.cpp
@@ -7,6 +7,17 @@ Vec::Vec(float x, float y, float z) : Tuple(x, y, z, 0.0f) {};
 Vec::Vec(const Tuple& tup) : Tuple(tup) {};
 // std::move is needed because a named rvalue reference (like 'tup') is treated as an lvalue, 
 //requiring a cast to rvalue for the move constructor.
+//A named rvalue reference behaves as an lvalue because it now has an identity (its name) 
+//and can be referred to multiple times.
+
+//Example:
+// Tuple tup;
+// Vec(std::move(tup))
+// if inside Vec constructor:
+// 1. Tuple(tup) is called -> Ownership of tup transfers from global scope to Vec, but is not transferred furthur 
+//      to the Tuple constructor. A new copy of tup will be created inside Tuple constructor, and the Vec will retain ownership
+//      of tup.
+// 2. Tuple(std::move(tup)) is called -> Ownership of tup is transferred from global scope to Vec to Tuple.
 Vec::Vec(Tuple&& tup) : Tuple(std::move(tup)) {};
 
 float Vec::magnitude() const{
