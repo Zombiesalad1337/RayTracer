19b20e697fb087ecfe3438c4062f78a581f4e30b
Zombiesalad1337 <rohit01031999@gmail.com>
2025-11-26 01:25:25 +0530

    cwd agnostic scripts

--- clean.sh
+++ clean.sh
@@ -3,27 +3,22 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
+CMAKE_DIR="$(pwd)"
 
-CURR_DIR=$(pwd)
-CMAKE_DIR="/mnt/d/notes2/sk/RayTracer"
-cd "${CMAKE_DIR}/build/"
-
-
-
-if [ $? -ne 0 ]; then
+cd "${CMAKE_DIR}/build/" || {
     echo "${RED}ERROR cding into build dir"
     echo "EXITING SCRIPT${RESET}"
     exit 1
-fi
+}
+
 pwd
 
 make clean
 
 read -p "${GREEN}About to NUKE $(pwd). ARE YOU SURE? " -n 1 -r
 echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    rm -v -r "${CMAKE_DIR}/build/" 
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rm -v -r "${CMAKE_DIR}/build/"
 fi
-mkdir "${CMAKE_DIR}/build"
 
+mkdir "${CMAKE_DIR}/build"
--- run.sh
+++ run.sh
@@ -3,20 +3,16 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
-CURR_DIR=$(pwd)
+CMAKE_DIR="$(pwd)"
 
-CMAKE_DIR="/mnt/d/notes2/sk/RayTracer"
-cd "${CMAKE_DIR}/build/"
-
-if [ $? -ne 0 ]; then
+cd "${CMAKE_DIR}/build/" || {
     echo "${RED}ERROR cding into build dir"
     echo "EXITING SCRIPT${RESET}"
     exit 1
-fi
+}
 
 pwd
 
-cmake ${CMAKE_DIR}
+cmake "${CMAKE_DIR}"
 make -j4 --directory "${CMAKE_DIR}/build/"
-echo "RUNNING MAIN"
-${CMAKE_DIR}/bin/main
+"${CMAKE_DIR}/bin/main"
